71c0b10dd520efe0cfa9c67d86b44ec5cb02d8b1
Asia <js.abramczyk@gmail.com>
2024-02-18 18:16:57 +0100

    Added create dropdown

--- src/components/layout/nav/Nav.tsx
+++ src/components/layout/nav/Nav.tsx
@@ -15,23 +15,31 @@ const Nav = () => {
 			</a>
 			<div className='nav__links'>
 				<h3 className='nav__links-heading'>Overview</h3>
-				<a href='#' className='nav__link nav__link--active'>
+				<a href='#' className='nav__link'>
 					<HomeIcon />
 					<p className='nav__link-title'>Home</p>
 				</a>
-				<a href='#' className='nav__link'>
+				<p className='nav__link nav__link--create'>
 					<AddIcon />
 					<p className='nav__link-title'>Create</p>
-				</a>
-				<a href='#' className='nav__link nav__link--active'>
-					<TaskListIcon />
-					<p className='nav__link-title '>Assigned tasks</p>
-				</a>
+					<div className='nav__link--create-dropdown'>
+						<a href='#' className='nav__link--create-dropdown-link'>
+							Create a task
+						</a>
+						<a href='#' className='nav__link--create-dropdown-link'>
+							Create a board
+						</a>
+					</div>
+				</p>
 				<a href='#' className='nav__link'>
+					<TaskListIcon />
+					<p className='nav__link-title'>Assigned tasks</p>
+				</a>
+				<a href='#' className='nav__link  '>
 					<BoardIcon />
 					<p className='nav__link-title'>Kanban boards</p>
 				</a>
-				<a href='#' className='nav__link'>
+				<a href='#' className='nav__link nav__link--active'>
 					<SettingsIcon />
 					<p className='nav__link-title'>Settings</p>
 				</a>
