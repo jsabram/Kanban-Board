399abcb54ed4ced19560e114fe318449b6b511fc
Asia <js.abramczyk@gmail.com>
2024-02-25 14:35:49 +0100

    Added pages components

---
+++ src/components/page-coponents/Task.tsx
@@ -0,0 +1,7 @@
+import './Task.scss';
+
+const Task = () => {
+	return <div>Task</div>;
+};
+
+export default Task;
---
+++ src/components/page-coponents/TaskSelection.tsx
@@ -0,0 +1,7 @@
+import './TaskSelection.scss';
+
+const TaskSelection = () => {
+	return <div>Task Selection</div>;
+};
+
+export default TaskSelection;
